daee2e69f34e252901035fd72e8ac98425747e10
Alejandro Falkowski <alexrfalkowski@gmail.com>
2024-07-09 15:59:04 +0200

    feat(http): remove grpc (#2)

--- server/health/module.go
+++ server/health/module.go
@@ -2,13 +2,15 @@ package health
 
 import (
 	"github.com/alexfalkowski/go-service/health"
-	"github.com/alexfalkowski/web/server/health/transport/grpc"
 	"github.com/alexfalkowski/web/server/health/transport/http"
 	"go.uber.org/fx"
 )
 
 // Module for fx.
 var Module = fx.Options(
-	fx.Provide(http.NewHealthObserver), fx.Provide(http.NewLivenessObserver), fx.Provide(http.NewReadinessObserver),
-	fx.Provide(grpc.NewObserver), fx.Provide(NewRegistrations), health.Module,
+	fx.Provide(http.NewHealthObserver),
+	fx.Provide(http.NewLivenessObserver),
+	fx.Provide(http.NewReadinessObserver),
+	fx.Provide(NewRegistrations),
+	health.Module,
 )
