c2a2a8da537a650064a4f834f315594cf70a676a
Alex Falkowski <alexrfalkowski@gmail.com>
2024-07-10 14:58:57 +0200

    feat(http): add books

--- server/v1/transport/http/views.go
+++ server/v1/transport/http/views.go
@@ -6,5 +6,7 @@ import (
 
 // Views for HTTP.
 //
+//go:embed root.tmpl.html
 //go:embed home.tmpl.html
+//go:embed books.tmpl.html
 var Views embed.FS
--- test/features/step_definitions/server/v1/transport/http/web_steps.rb
+++ test/features/step_definitions/server/v1/transport/http/web_steps.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+When('I visit root') do
+  opts = {
+    headers: {
+      request_id: SecureRandom.uuid, user_agent: 'Web-client/1.0 HTTP/1.0'
+    },
+    read_timeout: 10, open_timeout: 10
+  }
+
+  @response = Web::V1.server_http.root(opts)
+end
+
 When('I visit home') do
   opts = {
     headers: {
@@ -11,9 +22,34 @@ When('I visit home') do
   @response = Web::V1.server_http.home(opts)
 end
 
-Then('I should be at home') do
+When('I visit books') do
+  opts = {
+    headers: {
+      request_id: SecureRandom.uuid, user_agent: 'Web-client/1.0 HTTP/1.0'
+    },
+    read_timeout: 10, open_timeout: 10
+  }
+
+  @response = Web::V1.server_http.books(opts)
+end
+
+Then('I should see root') do
   expect(@response.code).to eq(200)
 
   html = Nokogiri::HTML.parse(@response.body)
   expect(html.title).to eq('Lean Thoughts')
+end
+
+Then('I should see home') do
+  expect(@response.code).to eq(200)
+
+  html = Nokogiri::HTML.parse(@response.body)
+  expect(html.text).to include('Lean Thinking')
+end
+
+Then('I should see books') do
+  expect(@response.code).to eq(200)
+
+  html = Nokogiri::HTML.parse(@response.body)
+  expect(html.text).to include('Kanban')
 end
