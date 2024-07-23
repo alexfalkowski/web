0feeedcfc76b487adf6f3fdfc5c52444c2c10b8f
Alex Falkowski <alexrfalkowski@gmail.com>
2024-07-23 13:52:28 +0200

    feat(deps): upgraded github.com/alexfalkowski/go-service to v1.251.0

--- server/site/home/home.go
+++ server/site/home/home.go
@@ -4,14 +4,13 @@ import (
 	"context"
 	"html/template"
 	"io/fs"
-	"net/http"
 
 	"github.com/alexfalkowski/go-service/net/http/mvc"
 )
 
 // Register home.
 func Register(fs fs.FS) {
-	mvc.Route("GET /home", func(_ context.Context, _ *http.Request, _ http.ResponseWriter) *mvc.Result {
+	mvc.Route("GET /home", func(_ context.Context) *mvc.Result {
 		v := template.Must(template.ParseFS(fs, "home/view.html"))
 		r := mvc.NewResult(nil, v)
 
--- server/site/root/root.go
+++ server/site/root/root.go
@@ -4,14 +4,13 @@ import (
 	"context"
 	"html/template"
 	"io/fs"
-	"net/http"
 
 	"github.com/alexfalkowski/go-service/net/http/mvc"
 )
 
 // Register root.
 func Register(fs fs.FS) {
-	mvc.Route("GET /", func(_ context.Context, _ *http.Request, _ http.ResponseWriter) *mvc.Result {
+	mvc.Route("GET /", func(_ context.Context) *mvc.Result {
 		v := template.Must(template.ParseFS(fs, "root/view.html"))
 		r := mvc.NewResult(nil, v)
 
