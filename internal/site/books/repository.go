e73e90940cc74a6039b46205abfbd371e4d3741c
Alex Falkowski <alexrfalkowski@gmail.com>
2025-04-28 10:25:50 +0200

    feat(books): use ptr package

--- internal/site/books/repository.go
+++ internal/site/books/repository.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/alexfalkowski/go-service/encoding/yaml"
 	"github.com/alexfalkowski/go-service/runtime"
+	"github.com/alexfalkowski/go-service/types/ptr"
 	"github.com/alexfalkowski/web/internal/site/meta"
 )
 
@@ -34,17 +35,16 @@ func (r *FSRepository) GetBooks() *Model {
 	books, err := fs.ReadFile(r.filesystem, "books/books.yaml")
 	runtime.Must(err)
 
-	var m Model
-	ptr := &m
+	model := ptr.Zero[Model]()
 
-	err = r.enc.Decode(bytes.NewBuffer(books), ptr)
+	err = r.enc.Decode(bytes.NewBuffer(books), model)
 	runtime.Must(err)
 
-	slices.SortFunc(ptr.Books, func(a, b *Book) int {
+	slices.SortFunc(model.Books, func(a, b *Book) int {
 		return cmp.Compare(a.Title, b.Title)
 	})
 
-	ptr.Info = r.info
+	model.Info = r.info
 
-	return ptr
+	return model
 }
